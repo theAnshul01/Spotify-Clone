dc5b522eafa799a689ca044e2a3140d13e1c643e
Anshul <anshulporwal2001@gmail.com>
2023-07-20 10:30:15 +0530

    before final changes

--- script.js
+++ script.js
@@ -7,28 +7,27 @@ let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
+let songItems = Array.from(document.getElementsByClassName("songItem"));
 
-let song = [
-    {songName:"Warriyo - Mortals (feat. Laura Brehm) [NCS Release]" , filePath:"songs/1.mp3",coverPath:"covers/1.jpg"},
-    {songName:"Cielo - Huma Huma" , filePath:"songs/2.mp3",coverPath:"covers/2.jpg"},
-    {songName:"Deaf-Kev - Invincible [NCS Release]-320k" , filePath:"songs/3.mp3",coverPath:"covers/3.jpg"},
-    {songName:"Different heaven & Eh!DE - my heart [NCS Release]-320k" , filePath:"songs/4.mp3",coverPath:"covers/4.jpg"},
-    {songName:"Janji-Heroes-Tonight-feat-Johning-NCS-Release " , filePath:"songs/5.mp3",coverPath:"covers/5.jpg"},
-    {songName:"Random 1" , filePath:"songs/6.mp3",coverPath:"covers/6.jpg"},
-    {songName:"Random-2" , filePath:"songs/7.mp3",coverPath:"covers/7.jpg"},
-    {songName:"Random-3" , filePath:"songs/8.mp3",coverPath:"covers/8.jpg"},
-    {songName:"Random-4" , filePath:"songs/9.mp3",coverPath:"covers/9.jpg"}
+let songs = [
+    {songName:"Warriyo - Mortals [NCS Release]" , filePath:"songs/1.mp3", coverPath:"images/covers/1.jpg"},
+    {songName:"Cielo - Huma Huma" , filePath:"songs/2.mp3",coverPath:"images/covers/2.jpg"},
+    {songName:"Deaf-Kev - Invincible [NCS Release]-320k" , filePath:"songs/3.mp3",coverPath:"images/covers/3.jpg"},
+    {songName:"Different heaven & Eh!DE" , filePath:"songs/4.mp3",coverPath:"images/covers/4.jpg"},
+    {songName:"Janji-Heroes-Tonight-feat-Johning-NCS-Release " , filePath:"songs/5.mp3",coverPath:"images/covers/5.jpg"},
+    {songName:"Bhula Dena" , filePath:"songs/6.mp3",coverPath:"images/covers/6.jpg"},
+    {songName:"Zihaal-E-mishkil" , filePath:"songs/7.mp3",coverPath:"images/covers/7.jpg"},
+    {songName:"Raabta" , filePath:"songs/8.mp3",coverPath:"images/covers/8.jpg"},
+    {songName:"Tere Vaste" , filePath:"songs/9.mp3",coverPath:"images/covers/9.jpg"}
 ]
 
+songItems.forEach((element, i)=>{
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerHTML = songs[i].songName;
+    
+})
+
 // handle play/pause click
-// backward.addEventListener('click', ()=>{
-//     if(songIndex != 0){
-//         songIndex--;
-
-//     }
-// })
-
-
 
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime <=0){
@@ -58,4 +57,14 @@ audioElement.addEventListener('timeupdate', ()=>{
 
 myProgressBar.addEventListener('change', ()=>{
     audioElement.currentTime = (myProgressBar.value * audioElement.duration /100);
+})
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        console.log(e.target);
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-circle-pause')
+        
+
+    })
 })
